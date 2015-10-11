c302ac8b42a736b13ea2a20c0793cd1ba942ae80
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-10-11 15:15:43 -0500

    proper postgres query for the database achieved

--- models/database.js
+++ models/database.js
@@ -3,79 +3,31 @@ var connectionString = 'postgres://localhost:5432/premise';
 
 pg.connect(connectionString, function (err, client, done) {
   if(err) {
-    return console.error('error fetching client from pool', err);
+    return console.error('error fetching Everything from pool', err);
   }
-  client.query('SELECT * FROM Clients', function(err, result) {
-
-    if(err) {
-      return console.error('error running Clients query', err);
-    }
-    console.log("==========");
-    console.log('Clients');
-    console.log("==========");
-
-    result.rows.forEach(function (row) {
-      console.log(row);
-    })
-
-    client.end();
-  });
-})
-
-pg.connect(connectionString, function (err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT * FROM Stakeholders', function(err, result) {
-
-    if(err) {
-      return console.error('error running Stakeholders query', err);
-    }
-    console.log("============");
-    console.log('Stakeholders');
-    console.log("============");
-
-    result.rows.forEach(function (row) {
-      console.log(row);
-    })
-
-    client.end();
-  });
-})
-
-pg.connect(connectionString, function (err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT * FROM Products', function(err, result) {
-
-    if(err) {
-      return console.error('error running Products query', err);
-    }
-    console.log("========");
-    console.log('Products');
-    console.log("========");
-
-    result.rows.forEach(function (row) {
-      console.log(row);
-    })
-
-    client.end();
-  });
-})
-
-pg.connect(connectionString, function (err, client, done) {
-  if(err) {
-    return console.error('error fetching product_details from pool', err);
-  }
-  client.query('SELECT * FROM Product_details', function(err, result) {
+  client.query('SELECT '+
+                'product_details.productid, '+
+                'products.productname, ' +
+                'stakeholdername, '+
+                'contactinfo, '+
+                'clientname '+
+              'FROM '+
+                'product_details '+
+              'INNER JOIN '+
+                'stakeholders ON product_details.stakeholderid = stakeholders.stakeholderid '+
+              'INNER JOIN '+
+                'clients ON product_details.clientid = clients.clientid '+
+              'INNER JOIN '+
+                'products ON product_details.productid = products.productid '+
+              'ORDER BY '+
+                'productid', function(err, result) {
 
     if(err) {
       return console.error('error running Product_details query', err);
     }
-    console.log("========");
-    console.log('Product Details');
-    console.log("========");
+    console.log("===========");
+    console.log('Final Query');
+    console.log("===========");
 
     result.rows.forEach(function (row) {
       console.log(row);
