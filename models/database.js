f54a471530846eeddeb76881624534bf3c7ffd6a
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-10-11 12:06:20 -0500

    fixed relational database & ERM & calling data to node console

--- models/database.js
+++ models/database.js
@@ -1,7 +1,86 @@
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/premise';
 
-var client = new pg.Client(connectionString);
-client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', function() { client.end(); });
+pg.connect(connectionString, function (err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM Clients', function(err, result) {
+
+    if(err) {
+      return console.error('error running Clients query', err);
+    }
+    console.log("==========");
+    console.log('Clients');
+    console.log("==========");
+
+    result.rows.forEach(function (row) {
+      console.log(row);
+    })
+
+    client.end();
+  });
+})
+
+pg.connect(connectionString, function (err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM Stakeholders', function(err, result) {
+
+    if(err) {
+      return console.error('error running Stakeholders query', err);
+    }
+    console.log("============");
+    console.log('Stakeholders');
+    console.log("============");
+
+    result.rows.forEach(function (row) {
+      console.log(row);
+    })
+
+    client.end();
+  });
+})
+
+pg.connect(connectionString, function (err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM Products', function(err, result) {
+
+    if(err) {
+      return console.error('error running Products query', err);
+    }
+    console.log("========");
+    console.log('Products');
+    console.log("========");
+
+    result.rows.forEach(function (row) {
+      console.log(row);
+    })
+
+    client.end();
+  });
+})
+
+pg.connect(connectionString, function (err, client, done) {
+  if(err) {
+    return console.error('error fetching product_details from pool', err);
+  }
+  client.query('SELECT * FROM Product_details', function(err, result) {
+
+    if(err) {
+      return console.error('error running Product_details query', err);
+    }
+    console.log("========");
+    console.log('Product Details');
+    console.log("========");
+
+    result.rows.forEach(function (row) {
+      console.log(row);
+    })
+
+    client.end();
+  });
+});
